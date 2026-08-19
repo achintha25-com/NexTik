eb8deaf114cec153780618fea4a9e53eb6720514
Your Name <you@example.com>
2026-08-19 23:42:38 +0530

    updated

--- includes/templates/partials/errors.php
+++
@@ -1,6 +0,0 @@
-<?php if (! empty($errors)): ?>
-    <div class="alert alert-error" role="alert">
-        <strong>Please correct the following:</strong>
-        <ul><?php foreach ($errors as $error): ?><li><?= e($error) ?></li><?php endforeach; ?></ul>
-    </div>
-<?php endif; ?>
